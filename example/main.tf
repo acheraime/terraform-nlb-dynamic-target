ef8f6f0c7b4a03722b383100414b68b3002b3333
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-08 18:19:08 -0400

    Add load balancer and company
    Former-commit-id: 154600fd09ebcd497c6b3b2f0195b8c7f0dac2b3

--- example/main.tf
+++ example/main.tf
@@ -23,3 +23,38 @@ module "rds" {
 
     depends_on = [aws_db_subnet_group.example]
 }
+
+# Loadbalancer and company
+resource "aws_lb" "example" {
+    name = "postgres-example"
+    internal = true
+    load_balancer_type = "network"
+    subnets = [aws_subnet.a.id, aws_subnet.b.id]
+
+    tags = {
+        Name = "postgres-example"
+    }
+}
+
+resource "aws_lb_listener" "example" {
+    load_balancer_arn = aws_lb.example.arn
+    port = local.port
+    protocol = "TCP"
+    default_action {
+        target_group_arn = aws_lb_target_group.example.arn
+        type = "forward"
+    }
+}
+
+resource "aws_lb_target_group" "example" {
+    name ="postgres-example"
+    port = local.port
+    protocol = "TCP"
+    target_type = "ip"
+    vpc_id = aws_vpc.example.id
+}
+
+resource "aws_lb_target_group_attachment" "example" {
+    target_group_arn = aws_lb_target_group.example.arn
+    target_id = "192.168.1.67"
+}
