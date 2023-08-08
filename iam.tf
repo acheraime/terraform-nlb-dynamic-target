452e526300ba14a006d28ba63f0c07e1fba46481
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-08 12:01:53 -0400

    Adding extra role policy
    Former-commit-id: 585db42ff63335b81589dea9de9eb89427909236

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 
-# Role policy document
+# Role STS policy document
 data "aws_iam_policy_document" "this" {
     statement {
         sid = "AllowAsume"
@@ -23,4 +23,42 @@ resource "aws_iam_role" "this" {
 resource "aws_iam_role_policy_attachment" "lambda" {
     role = aws_iam_role.this.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Extra policies
+data "aws_iam_policy_document" "extra" {
+    statement {
+        sid = "AllowCreateNetIfaces"
+        actions = [
+            "elasticloadbalancing:RegisterTargets",
+            "elasticloadbalancing:DeregisterTargets",
+            "elasticloadbalancing:DescribeTargetHealth",
+            "ec2:CreateNetworkInterface",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DeleteNetworkInterface"
+        ]
+        effect = "Allow"
+        resources = ["*"] #TODO: Too permissive, add only arns of target load  balancer
+    }
+
+    statement {
+        sid = "AllowWriteLogs"
+        actions = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+        ]
+        effect = "Allow"
+        resources = ["arn:aws:logs:*:*:*"]
+    }
+}
+
+resource "aws_iam_policy" "extra" {
+    name = "nlb-target-update-policy"
+    policy = data.aws_iam_policy_document.extra.json
+}
+
+resource "aws_iam_role_policy_attachment" "extra" {
+    role = aws_iam_role.this.name
+    policy_arn = aws_iam_policy.extra.arn
 }
