00d54f81da7b5a48149a81c30eb4c4d5d29f5084
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-09 17:12:42 -0400

    Declare runtime local
    Former-commit-id: 7ea882a6442e5e7726ab441aba1b09d1494d5d2a

--- data.tf
+++ data.tf
@@ -3,6 +3,7 @@ data "archive_file" "source" {
     type = "zip"
     source_dir = "${path.module}/lambda"
     output_path = "${path.module}/${local.lambda_file_name}"
+    excludes = ["${path.module}/lambda/lambda-venv"]
 }
 
 # Bucket lookup
--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
   function_name = "nlb-target-update"
   lambda_file_name = "nlb-target.zip"
+  runtime = "python3.11"
 }
