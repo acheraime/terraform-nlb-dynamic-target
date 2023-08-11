6abc6ebe1f82863ae1ea110ba237a6773cf2134e
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-11 11:51:03 -0400

    declare more locals
    Former-commit-id: 9d6560b5b612758e58f9f05c4fcf5c6ece811ac4

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
-  function_name = "nlb-target-update"
-  lambda_file_name = "nlb-target.zip"
+  resource_prefix = "nlb-target"
+  function_name = var.function_name != null ? var.function_name : "${local.resource_prefix}-updater"
+  lambda_file_name = "${local.function_name}-source.zip"
   runtime = "python3.11"
   implicit_invoke = lower(var.lambda_log_level) == "debug" ? 1 : 0
 }
