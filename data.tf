3e64afb5fa4a5f3a08ce2a4bd23c6ad63ccb3041
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-11 11:18:31 -0400

    Input validation rules
    Former-commit-id: ed8480fdb888c94bb88300a19ef9930e589f342a

--- data.tf
+++ data.tf
@@ -1,12 +1,7 @@
-# Archive source folder
+# Zip archive source code folder
 data "archive_file" "source" {
     type = "zip"
     source_dir = "${path.module}/lambda"
     output_path = "${path.module}/${local.lambda_file_name}"
     excludes = ["${path.module}/lambda/lambda-venv"]
-}
-
-# Bucket lookup
-data "aws_s3_bucket" "bucket" {
-    bucket = var.bucket_name
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "bucket_name" {
-    type = string
-    description = "Name of the S3 bucket to upload lambda code"
-    default = "zmn-lambda-dev"
-}
-
 variable "db_instance_ids" {
     type = list(string)
     description = "List of RDS instances ID"
@@ -23,16 +17,32 @@ variable "lambda_log_level" {
     type = string
     description = "Log verbosity level of the lambda function"
     default = "info"
+    validation {
+        condition = can(regex("[debug|info|warning|error|critical]", var.lambda_log_level))
+        error_message = "Not a valid log level."
+    }
 }
 
 variable "max_retries" {
     type = number
-    description = "Maximum times to retry a failed remote call"
+    description = "Maximum times to retry a failed remote call within the range [1-10]"
     default = 3
+    validation {
+        condition = (
+            var.max_retries >= 1 && var.max_retries <= 10
+        )
+        error_message = "We expect max_retries value to be a positive number between 1 - 10."
+    }
 }
 
 variable "retry_interval_seconds" {
     type = number
     description = "Interval time in seconds to wait before retry a failed remote call"
     default = 5
+    validation {
+        condition = (
+            var.retry_interval_seconds >= 5 && var.retry_interval_seconds <= 1800
+        )
+        error_message = "The retry interval in seconds must be between 5 and 1800."
+    }
 }
