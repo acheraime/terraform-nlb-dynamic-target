904ff181182727cab81c2c5f3ab9a25677462e7d
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-11 12:26:09 -0400

    default_rds_eventss and rds_events locals
    Former-commit-id: 5774799ed15b0065146585aa76c9e7718e723b26

--- locals.tf
+++ locals.tf
@@ -1,7 +1,16 @@
 locals {
-  resource_prefix = "nlb-target"
-  function_name = var.function_name != null ? var.function_name : "${local.resource_prefix}-updater"
+  resource_prefix  = "nlb-target"
+  function_name    = var.function_name != null ? var.function_name : "${local.resource_prefix}-updater"
   lambda_file_name = "${local.function_name}-source.zip"
-  runtime = "python3.11"
-  implicit_invoke = lower(var.lambda_log_level) == "debug" ? 1 : 0
+  runtime          = "python3.11"
+  implicit_invoke  = lower(var.lambda_log_level) == "debug" ? 1 : 0
+  default_rds_events = [
+    "availability",
+    "failover",
+    "failure",
+    "maintenance",
+    "recovery",
+    "restoration",
+  ]
+  rds_events = distinct(concat(local.default_rds_events, var.extra_rds_events))
 }
--- sns.tf
+++ sns.tf
@@ -1,27 +1,18 @@
 # Create SNS topic
 resource "aws_sns_topic" "this" {
-    name = "database-topic"
+  name = "${local.resource_prefix}-sns-topic"
 }
 
 resource "aws_db_event_subscription" "this" {
-    name = "database-topic-subscription"
-    sns_topic = aws_sns_topic.this.arn
-
-    source_type = "db-instance"
-    source_ids = var.db_instance_ids
-
-    event_categories = [
-        "availability",
-        "failover",
-        "failure",
-        "maintenance",
-        "recovery",
-        "restoration",
-    ]
+  name             = "${local.resource_prefix}-database-events-subscription"
+  sns_topic        = aws_sns_topic.this.arn
+  source_type      = "db-instance"
+  source_ids       = var.db_instance_ids
+  event_categories = local.rds_events
 }
 
 resource "aws_sns_topic_subscription" "this" {
-    topic_arn = aws_sns_topic.this.arn
-    protocol = "lambda"
-    endpoint = aws_lambda_function.this.arn
+  topic_arn = aws_sns_topic.this.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.this.arn
 }
