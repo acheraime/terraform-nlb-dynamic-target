425c90fef50a0c510ca4a9c234eb04b467287b9d
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-13 23:57:50 -0400

    Add logger to LBTargetGroup class

--- lambda/utils/loadbalancer.py
+++ lambda/utils/loadbalancer.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import dns.resolver
+import logging
 
 from collections import Counter
 from dns.resolver import NXDOMAIN, NoNameservers
@@ -36,6 +37,7 @@ class LBTargetGroup:
     current_target_ids: list = field(default_factory=list)
     new_target_ids: list = field(default_factory=list)
     to_be_updated: bool = False
+    logger: logging.Logger = logging.getLogger()
 
     @with_backoff_decorator((ClientError,), tries=MaxRetries, delay=RetyIntervalSeconds)
     def __post_init__(self):
@@ -46,7 +48,8 @@ class LBTargetGroup:
             lbtg_attr = client.describe_target_health(TargetGroupArn=self.arn)
             for target in lbtg_attr['TargetHealthDescriptions']:
                 self.current_target_ids.append(target['Target']['Id'])
-        except ClientError:
+        except ClientError as err:
+           self.logger.error(f"unable to instantiate the LBTargetGroup class: {err}")
            raise 
     
     @with_backoff_decorator((NXDOMAIN, NoNameservers), tries=MaxRetries, delay=RetyIntervalSeconds)
@@ -59,10 +62,11 @@ class LBTargetGroup:
             for ip in r:
                 self.new_target_ids.append(ip.to_text())
         except (NXDOMAIN, NoNameservers) as err:
+            self.logger.error(f"name resolution failure: {err}")
             raise Exception(err)
         
         if Counter(self.current_target_ids) != Counter(self.new_target_ids):
-            # The old targets are different then the new ones
+            # The old targets are different than the new ones
             self.to_be_updated = True
 
     @with_backoff_decorator((ClientError,), tries=MaxRetries, delay=RetyIntervalSeconds)
@@ -80,6 +84,7 @@ class LBTargetGroup:
                     Targets=bad_target_ids
                 )
             except ClientError as err:
+                self.logger.error(f"fail to deregister target(s) {bad_target_ids} from LB: {err}")
                 raise Exception(err)
         
         # register new targets
@@ -90,6 +95,7 @@ class LBTargetGroup:
                 Targets=new_target_ids
             )
         except ClientError as err:
+            self.logger.error(f"fail to register new target(s) {new_target_ids} to LB: {err}")
             raise Exception(err)
         
         return True
