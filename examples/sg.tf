ac7b201d4497912d3294ff1c8c72268866c1b05a
Adolphe Cher-Aime <acheraime@gmail.com>
2023-08-17 07:59:14 -0400

    add security group

--- examples/sg.tf
+++ examples/sg.tf
@@ -22,3 +22,37 @@ resource "aws_security_group" "example" {
     Name = "Allow Ingress Postgres"
   }
 }
+
+resource "aws_security_group" "this" {
+  name        = "example-lambda-outbound"
+  description = "Allow lambda outbound access to dns and internet"
+  vpc_id      = aws_vpc.example.id
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
